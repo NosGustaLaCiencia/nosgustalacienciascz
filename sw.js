f594f64c2d90cda825124328aab0822c8cc60e53
Pericena <pericena12@gmail.com>
2023-07-07 04:18:57 -0400

    deploy notificacion

--- sw.js
+++ sw.js
@@ -2,7 +2,6 @@ const CACHE_NAME = 'v1_pwa_app_cache';
 const urlsToCache = [
   './',
   'index.html',
-  '404.html',
   'assets/css/style.css',
   'assets/js/script.js',
   'js/script2.js',
@@ -20,14 +19,17 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+
   event.waitUntil(
     caches.keys()
-      .then(cacheNames => {
-        return Promise.all(
-          cacheNames.filter(cacheName => cacheName !== CACHE_NAME)
-            .map(cacheName => caches.delete(cacheName))
-        );
-      })
+      .then(cacheNames => Promise.all(
+        cacheNames.map(cacheName => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      ))
       .then(() => self.clients.claim())
   );
 });
@@ -36,31 +38,13 @@ self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => {
-        // Devuelve la respuesta almacenada en caché si está disponible
-        // o muestra una página de respaldo si no hay conexión a Internet
-        return response || caches.match('404.html');
+        return response || fetch(event.request);
       })
       .catch(error => console.log('Falló al solicitar recursos', error))
   );
 });
 
-self.addEventListener('notificationclick', event => {
-  event.notification.close();
 
-  // Realiza la acción deseada al hacer clic en la notificación
-  // Por ejemplo, redirigir a una URL específica o ejecutar una función
-});
 
-self.addEventListener('push', event => {
-  if (event.data) {
-    const options = {
-      body: event.data.text(),
-      icon: 'assets/images/logo/s2.png',
-    //   vibrate: [200, 100, 200, 100, 200, 100, 200],
-    };
 
-    event.waitUntil(
-      self.registration.showNotification('¡Hola!', options)
-    );
-  }
-});
+
