fa0834cf9cd447b1a8d0dd5a561a4a33670ec358
Pericena <pericena12@gmail.com>
2023-07-10 14:28:14 -0400

    etiquetas

--- sw.js
+++ sw.js
@@ -1,68 +1,85 @@
-;
-//asignar un nombre y versión al cache
-const CACHE_NAME = 'v1_pwa_app_cache',
-  urlsToCache = [
-    './',
-    'index.html',
-    'assets/css/style.css',
-    'assets/js/script.js',
-    'js/script2.js',
-    'assets/images/logo/s2.png',
-    'assets/images/logo/s2.png',
-  ]
+const CACHE_NAME = 'v1_pwa_app_cache';
+const urlsToCache = [
+  './',
+  'index.html',
+  'assets/css/style.css',
+  'assets/js/script.js',
+  'js/script2.js',
+  'assets/images/logo/s2.png',
+  'assets/images/.*',
+  'assets/sonido/musica.mp3',
+];
 
-  self.addEventListener("install", (e) => {
-    e.waitUntil(
-        caches.open(CACHE_NAME)//retorna una promesa
-            .then(cache =>{cache.addAll(CACHE_ELEMENTS)//al pasarle addAll hacemos que pase todas las ruta al serviceWorker y devuelve una promesa
-                .then( () => {
-                    self.skipWaiting()
-                }).catch(console.log)
-            }
-        )
-    )
-    
-  })
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache)
+          .then(() => {
+            self.skipWaiting();
+          })
+          .catch(console.log);
+      })
+  );
+});
 
-  
-
-
-//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
 self.addEventListener('activate', e => {
-  const cacheWhitelist = [CACHE_NAME]
+  const cacheWhitelist = [CACHE_NAME];
 
   e.waitUntil(
     caches.keys()
       .then(cacheNames => {
         return Promise.all(
           cacheNames.map(cacheName => {
-            //Eliminamos lo que ya no se necesita en cache
             if (cacheWhitelist.indexOf(cacheName) === -1) {
-              return caches.delete(cacheName)
+              return caches.delete(cacheName);
             }
           })
-        )
+        );
       })
-      // Le indica al SW activar el cache actual
       .then(() => self.clients.claim())
-  )
-})
+  );
+});
 
-//cuando el navegador recupera una url
 self.addEventListener('fetch', e => {
-  //Responder ya sea con el objeto en caché o continuar y buscar la url real
   e.respondWith(
     caches.match(e.request)
       .then(res => {
         if (res) {
-          //recuperar del cache
-          return res
+          return res;
         }
-        //recuperar de la petición a la url
         return fetch(e.request)
-      }).catch(err => console.log('Falló algo al solicitar recursos', err))
-  )
-})
+          .then(response => {
+            if (!response || response.status !== 200 || response.type !== 'basic') {
+              self.clients.matchAll().then(clients => {
+                clients.forEach(client => {
+                  client.postMessage('Falló algo al solicitar recursos: ' + e.request.url);
+                });
+              });
+              return response;
+            }
+            const responseToCache = response.clone();
+            caches.open(CACHE_NAME)
+              .then(cache => {
+                cache.put(e.request, responseToCache);
+              });
+            return response;
+          });
+      })
+      .catch(err => {
+        console.log('Falló algo al solicitar recursos:', err);
+        self.clients.matchAll().then(clients => {
+          clients.forEach(client => {
+            client.postMessage('Falló algo al solicitar recursos');
+          });
+        });
+      })
+  );
+});
 
-
-
+self.addEventListener('message', event => {
+  if (event.data === 'Falló algo al solicitar recursos') {
+    // Mostrar ventana o notificación de error al usuario
+    // ... tu código aquí ...
+  }
+});
